754fe3feff0cbb86a064f43ec516588e42dd1072
Rayhan-55 <asfyrayhan1106@gmail.com>
2025-09-11 22:29:18 +0600

    fleax class end

---
+++ lib/Flex/Fleaxiable_plus.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+class FleaxiablePlus extends StatefulWidget {
+  const FleaxiablePlus({super.key});
+
+  @override
+  State<FleaxiablePlus> createState() => _FleaxiablePlusState();
+}
+
+class _FleaxiablePlusState extends State<FleaxiablePlus> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("THis is tittle"),),
+      body: Center(
+        
+      ),
+    );
+  }
+}
